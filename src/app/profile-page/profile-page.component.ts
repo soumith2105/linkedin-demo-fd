3b8764cacff10e2d04cb876764b9e1e7d161db8d
soumith2105 <bannu.shaili@gmail.com>
2021-01-16 22:05:11 +0530

    Added Education Component

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Education{
+  institution: string;
+  duration: string;
+  startMonth: string;
+  endMonth: string;
+  description: string;
+  location: string;
+  course: string;
+}
+
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
@@ -7,10 +17,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
+  education: Array<Education> = [
+    {
+      institution: 'Narayana',
+      duration: '2 years',
+      startMonth: 'Jan 2019',
+      endMonth: 'Feb 2019',
+      description: 'Good College',
+      location: 'Hyderabad',
+      course: 'Intermediate',
+    },
+    {
+      institution: 'Narayana',
+      duration: '2 years',
+      startMonth: 'Jan 2019',
+      endMonth: 'Feb 2019',
+      description: 'Good College',
+      location: 'Hyderabad',
+      course: 'Intermediate',
+    },
+  ];
   constructor() {
   }
 
   ngOnInit(): void {
   }
+
+
+
 
 }
--- src/app/profile-page/profile-page.module.ts
+++ src/app/profile-page/profile-page.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { ProfilePageComponent } from './profile-page.component';
-import { ProfilePageRoutingModule } from './profile-page-routing.module';
+import {NgModule} from '@angular/core';
+import {ProfilePageComponent} from './profile-page.component';
+import {ProfilePageRoutingModule} from './profile-page-routing.module';
+import {CommonModule} from '@angular/common';
+import {EducationComponent} from '../education/education.component';
 
 @NgModule({
-  declarations: [ProfilePageComponent],
-  imports: [ProfilePageRoutingModule],
-  exports: [ProfilePageComponent],
+    declarations: [ProfilePageComponent, EducationComponent],
+    imports: [ProfilePageRoutingModule, CommonModule],
+    exports: [ProfilePageComponent],
 })
-export class ProfilePageModule {}
+export class ProfilePageModule {
+}
